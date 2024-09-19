c3b9b5c9586cfbd49aa855478460abdedf36785d
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-09-19 16:40:13 +0100

    fixed the error by downgrading the werkzeug{

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # install flask from pip3
 package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug' :
+  ensure   => '2.0.3',  # Pin the compatible version
+  provider => 'pip3',
+  require  => Package['flask'],  # Ensure Flask is installed first
+}
